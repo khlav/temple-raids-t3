c184702ecab41d7e4e71f171a2571fdc4b43413a
khlav <kirk.hlavka@gmail.com>
2025-01-10 13:52:55 -0500

    rm sample links from New

--- src/app/raids/new/page.tsx
+++ src/app/raids/new/page.tsx
@@ -8,14 +8,6 @@ import {redirect} from "next/navigation";
 export default async function RaidNewPage() {
   const session = await auth();
 
-  const sampleLinks = [
-    "https://vanilla.warcraftlogs.com/reports/kY1KV3jMgPzQaXFW",
-    "https://vanilla.warcraftlogs.com/reports/d3YDbmJq9RGtpPMj",
-    "https://vanilla.warcraftlogs.com/reports/TBPp7gZ8Ykdv3Cr1",
-    "https://vanilla.warcraftlogs.com/reports/dWL8TKrRFD9jkvAb",
-    "https://vanilla.warcraftlogs.com/reports/vVw7CH2RDc6tn4Pb",
-  ];
-
   if(!session?.user?.isAdmin) {
     redirect('/raids');
   }
@@ -26,9 +18,6 @@ export default async function RaidNewPage() {
         <div className="h-auto w-full pb-2">
           <CreateRaid />
         </div>
-        {sampleLinks.map((link, i) => (
-          <div className="text-xs text-secondary" key={link}> {link} </div>
-        ))}
       </main>
     </HydrateClient>
   );
