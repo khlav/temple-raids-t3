7bbcf2b39fe5b707144a53bb07177417c1de55b5
khlav <kirk.hlavka@gmail.com>
2025-01-10 12:31:12 -0500

    attempt fix on charpage

--- src/app/players/[characterId]/page.tsx
+++ src/app/players/[characterId]/page.tsx
@@ -1,15 +1,25 @@
-import {CharacterDetail, CharacterRaids} from "~/components/players/old/characterdetail";
+import {
+  CharacterDetail,
+  CharacterRaids,
+} from "~/components/players/old/characterdetail";
 
-export default async function PlayerPage({params} : {params: { characterId: number } }) {
+export default async function PlayerPage({ params }: {params: Promise<{characterId: number}>}) {
   const p = await params;
-  const characterId = parseInt(String(p.characterId)); // Access your dynamic URL parameter here (e.g., /raids/[[raidId]])
-
+  const characterId = parseInt(String(p.characterId));
   return (
     <div>
-      <div className="text-2xl">Character pages are in-progress.  Meanwhile, here's some raw data.</div>
-      <div className="text-muted-foreground pb-4">Character ID: {characterId}</div>
-      <CharacterDetail characterId={characterId} />
-      <CharacterRaids characterId={characterId} />
+      <div className="text-2xl">
+        Character pages are in-progress. Meanwhile, here is a bit of data.
+      </div>
+      <div className="text-muted-foreground pb-4">
+        Character ID: {characterId}
+      </div>
+      {characterId && (
+        <>
+          <CharacterDetail characterId={characterId} />
+          <CharacterRaids characterId={characterId} />
+        </>
+      )}
     </div>
   );
 }
