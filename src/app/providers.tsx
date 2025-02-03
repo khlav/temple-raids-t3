ab50227f8a4d9f9d4d1087e9f05d72b81dfc0dc0
khlav <kirk.hlavka@gmail.com>
2025-02-03 09:12:52 -0500

    add manual pageview event

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,9 +2,46 @@
 
 "use client";
 
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from "posthog-js/react";
+
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
-import { type ReactNode, useEffect } from "react";
+import { type ReactNode } from "react";
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+
+  // Track pageviews
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
+
+// Wrap this in Suspense to avoid the `useSearchParams` usage above
+// from de-opting the whole app into client-side rendering
+// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
+export default function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  );
+}
 
 export function PostHogProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
@@ -12,6 +49,8 @@ export function PostHogProvider({ children }: { children: ReactNode }) {
       api_host: "/e",
       ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: "identified_only",
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageleave: true, // Enable pageleave capture
     });
     if (
       location.hostname === "localhost" ||
@@ -21,5 +60,10 @@ export function PostHogProvider({ children }: { children: ReactNode }) {
     }
   }, []);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>;
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  );
 }
