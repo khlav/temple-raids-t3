f328cca4c7297b1201a8545e8ed1eb8dda9fb58d
khlav <kirk.hlavka@gmail.com>
2025-03-14 17:09:21 -0400

    update

--- src/components/dashboard/dashboard-banner.tsx
+++ src/components/dashboard/dashboard-banner.tsx
@@ -6,6 +6,7 @@ import React from "react";
 
 export default function DashboardBanner() {
   const loot_prio_url = "https://docs.google.com/spreadsheets/d/1OBcFgT1AXiPL3eW7x3yUx6EjsopPLlFMclph2OGRkXU/edit?gid=0#gid=0";
+  const raid_policy_channel_url = "https://discord.com/channels/1132586324264759390/1194046879508480100";
 
   return (
     <div className="pb-2">
@@ -41,31 +42,43 @@ export default function DashboardBanner() {
               In practice, this means raiders must participate in ~2 different 40-player raids (in different zones) on average each week to stay eligible.
             </div>
             <div className="pt-2">
-              <strong>How it works: </strong>
-              Raiders earn up to three (3) raid credits per week toward
-              attendance.
+              Note: If a raid is full, you can earn credit by being ready/available at raid time and contacting an officer.
             </div>
-            <ul className="list-disc pl-6">
-              <li>
-                AQ40, Naxx, and BWL each grant +1 credit (max once per zone per
-                week)
-              </li>
-              <li>MC grants +1/2 credit (max once per week)</li>
-            </ul>
-            <div className="pt-2">
-              Your % attendance over 6 weeks is your total credits divided by 18
-              (3 per week, 6 weeks).
-            </div>
-            <div className="text-xs italic pt-2">
-              Note: Alts count towards your overall attendance as a player. Check the{" "}
+            <div>
               <Link
-                href="/characters"
-                className="text-secondary-foreground hover:underline"
+                className="text-blue-500 hover:text-secondary-foreground hover:underline"
+                href={raid_policy_channel_url}
+                target="_blank"
               >
-                players
-              </Link>{" "}
-              list for raiding character associations, and talk to an officer for updates.
+                Learn more in Discord: <strong>Temple > #raid-policies</strong>
+              </Link>
             </div>
+            {/*<div className="pt-2">*/}
+            {/*  <strong>How it works: </strong>*/}
+            {/*  Raiders earn up to three (3) raid credits per week toward*/}
+            {/*  attendance.*/}
+            {/*</div>*/}
+            {/*<ul className="list-disc pl-6">*/}
+            {/*  <li>*/}
+            {/*    AQ40, Naxx, and BWL each grant +1 credit (max once per zone per*/}
+            {/*    week)*/}
+            {/*  </li>*/}
+            {/*  <li>MC grants +1/2 credit (max once per week)</li>*/}
+            {/*</ul>*/}
+            {/*<div className="pt-2">*/}
+            {/*  Your % attendance over 6 weeks is your total credits divided by 18*/}
+            {/*  (3 per week, 6 weeks).*/}
+            {/*</div>*/}
+            {/*<div className="text-xs italic pt-2">*/}
+            {/*  Note: Alts count towards your overall attendance as a player. Check the{" "}*/}
+            {/*  <Link*/}
+            {/*    href="/characters"*/}
+            {/*    className="text-secondary-foreground hover:underline"*/}
+            {/*  >*/}
+            {/*    players*/}
+            {/*  </Link>{" "}*/}
+            {/*  list for raiding character associations, and talk to an officer for updates.*/}
+            {/*</div>*/}
           </div>
         </CardContent>
       </Card>
