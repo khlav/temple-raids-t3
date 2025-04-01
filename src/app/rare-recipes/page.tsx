6ce4dfae39fd0d44a198b9170e3d4a92a4850904
khlav <kirk.hlavka@gmail.com>
2025-04-01 09:26:08 -0400

    add button on crafters

--- src/app/rare-recipes/page.tsx
+++ src/app/rare-recipes/page.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import {Separator} from "~/components/ui/separator";
 import {RecipesWithCrafters} from "~/components/rare-recipes/recipes-with-crafters";
+import {Button} from "~/components/ui/button";
+import Link from "next/link";
+import {auth} from "~/server/auth";
 
 export default async function RecipeManagerIndex() {
+  const session = await auth();
+
   return (
     <main className="w-full px-4">
-      <div className="mb-2 text-3xl font-bold tracking-tight">
-        Rare Recipes & Crafters
+      <div className="flex gap-4">
+        <div className="grow-0 pb-4 text-3xl font-bold">Rare Recipes & Crafters</div>
+        <div className="grow text-right">
+          {!!session?.user && (
+            <Button asChild className="accent-accent">
+              <Link href={`/characters`}> + Add recipes to a character</Link>
+            </Button>
+          )}
+        </div>
       </div>
-      <Separator className="my-2" />
-      <RecipesWithCrafters />
+      <Separator className="my-2"/>
+      <RecipesWithCrafters/>
     </main>
   );
 }
