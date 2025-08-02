b0b89a2a60782231afd82fbaf02a2ebebeb172ef
khlav <kirk.hlavka@gmail.com>
2025-08-01 21:15:56 -0400

    fix skeleton

--- src/components/characters/skeletons.tsx
+++ src/components/characters/skeletons.tsx
@@ -58,32 +58,46 @@ export function GenericCharactersTableSkeleton({
 }) {
   return (
     <>
-      <Table className="max-h-[400px]">
+      <Table className="max-h-[400px] w-full">
         <TableHeader>
           <TableRow>
-            <TableHead className="w-3/4">
+            <TableHead className="w-1/2">
               Characters
             </TableHead>
-            <TableHead className="w-1/3">Server</TableHead>
-            <TableHead className="w-1/3">Class</TableHead>
+            <TableHead className="w-1/4">Server</TableHead>
+            <TableHead className="w-16 text-center text-xs">MC</TableHead>
+            <TableHead className="w-16 text-center text-xs">BWL</TableHead>
+            <TableHead className="w-16 text-center text-xs">AQ40</TableHead>
+            <TableHead className="w-16 text-center text-xs">Naxx</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {Array.from({ length: rows }).map((_, rowIndex) => (
             <TableRow key={rowIndex}>
-              {Array.from({ length: 3 }).map((_, cellIndex) => {
-                // Create a deterministic pseudo-random value
-                const seed = rowIndex * 3 + cellIndex; // Unique seed per cell
-                const pseudoRandomWidth = 40 + ((seed * 45) % 31); // Generates widths between 40% and 80%
-                return (
-                  <TableCell key={cellIndex}>
-                    <Skeleton
-                      className="my-1 h-4 px-2"
-                      style={{ width: `${pseudoRandomWidth}%` }}
-                    />
-                  </TableCell>
-                );
-              })}
+              <TableCell>
+                <Skeleton
+                  className="my-1 h-4 px-2"
+                  style={{ width: `${40 + ((rowIndex * 45) % 31)}%` }}
+                />
+              </TableCell>
+              <TableCell>
+                <Skeleton
+                  className="my-1 h-4 px-2"
+                  style={{ width: `${40 + ((rowIndex * 45 + 1) % 31)}%` }}
+                />
+              </TableCell>
+              <TableCell className="text-center">
+                <Skeleton className="m-auto h-4 w-6" />
+              </TableCell>
+              <TableCell className="text-center">
+                <Skeleton className="m-auto h-4 w-6" />
+              </TableCell>
+              <TableCell className="text-center">
+                <Skeleton className="m-auto h-4 w-6" />
+              </TableCell>
+              <TableCell className="text-center">
+                <Skeleton className="m-auto h-4 w-6" />
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
